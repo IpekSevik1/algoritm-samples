8ccc5c45f6dce9a5f67b7647c5681bd071a7f958
Ipek Sevik <sevikipek3@gmail.com>
2024-02-28 12:51:33 +0300

    new class created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
+import basics.EvenNumbers
+
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    EvenNumbers().getEvenNumbers()
 }
